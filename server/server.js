79f5243f5ceb94ce74d6e0a0363a1b0a9f3839e1
Akhil Bhandaru <bhandaru.akhil@gmail.com>
2020-06-29 15:13:51 -0700

    minor

--- server/server.js
+++ server/server.js
@@ -4,13 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser);
 
-const server = app.listen(process.env.PORT || 8080, () => {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
-
 // GET tweets
 app.get('/api/tweets', (req, res) => {
   let tweets = ['a', 'b', 'c', 'd', 'e'];
-  res.status(200).json(tweets);
+  res.status(200).send('Hello world!');
 });
