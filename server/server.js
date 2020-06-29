f9b37736e7ce9572f3721f794aa88aba90ed9783
Akhil Bhandaru <bhandaru.akhil@gmail.com>
2020-06-29 15:26:49 -0700

    minor

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 8000;
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -7,5 +8,10 @@ app.use(bodyParser);
 // GET tweets
 app.get('/api/tweets', (req, res) => {
   let tweets = ['a', 'b', 'c', 'd', 'e'];
-  res.status(200).send('Hello world!');
+  res.status(200).send(tweets);
+});
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
